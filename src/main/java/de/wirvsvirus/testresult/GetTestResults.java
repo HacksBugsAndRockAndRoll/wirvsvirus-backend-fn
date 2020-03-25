038b68548499b98c4264f79f9434b5f8f345274b
Thomas <yddrass@gmail.com>
2020-03-25 22:30:39 +0100

    foo

--- src/main/java/de/wirvsvirus/testresult/GetTestResults.java
+++ src/main/java/de/wirvsvirus/testresult/GetTestResults.java
@@ -6,7 +6,9 @@ import org.bson.BsonDocument;
 import org.bson.BsonString;
 import org.bson.conversions.Bson;
 
-import com.fasterxml.jackson.databind.node.TextNode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -22,21 +24,28 @@ public class GetTestResults {
 
 	@FunctionName("tests")
 	public HttpResponseMessage run(@HttpTrigger(name = "req", methods = {
-			HttpMethod.GET }, authLevel = AuthorizationLevel.ANONYMOUS, route = "/{hash:alpha}") HttpRequestMessage<Optional<String>> request,
+			HttpMethod.GET }, authLevel = AuthorizationLevel.ANONYMOUS, route = "tests/{hash}") HttpRequestMessage<Optional<String>> request,
 			@BindingName("hash") String hash, final ExecutionContext context) {
 		context.getLogger().info("Java HTTP trigger processed a request.");
 
 		if (hash == null) {
 			return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("no hash given").build();
 		} else {
-			String uri = System.getenv("mongodb.uri");
+			String uri = System.getenv("mongodb_uri");
 			if (uri==null)
 				uri="mongodb://localhost=27017/fn-test";
 			try (MongoClient client = new MongoClient(uri)) {
 				Bson filter = new BsonDocument("_id", new BsonString(hash));
-				TestResult res = client.getDatabase("fn-test").getCollection("testresult").find(filter, TestResult.class).first();
+				TestResult res = new ObjectMapper().readValue(client.getDatabase("fn-test").getCollection("testresult").find(filter).first().toJson(),TestResult.class);
 				return request.createResponseBuilder(HttpStatus.OK).body(res).build();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
 	}
 }
