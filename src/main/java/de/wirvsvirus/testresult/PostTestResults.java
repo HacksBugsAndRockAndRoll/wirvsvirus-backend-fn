c26241f95002420aa6029297a79dbc31a780b49b
Thomas <yddrass@gmail.com>
2020-03-25 23:49:16 +0100

    bar

--- src/main/java/de/wirvsvirus/testresult/PostTestResults.java
+++ src/main/java/de/wirvsvirus/testresult/PostTestResults.java
@@ -2,12 +2,8 @@ package de.wirvsvirus.testresult;
 
 import java.util.Optional;
 
-import org.bson.BsonDocument;
-import org.bson.BsonString;
-import org.bson.conversions.Bson;
+import org.bson.Document;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -17,33 +13,35 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
-public class GetTestResults {
+public class PostTestResults {
 
 	@FunctionName("tests")
-	public HttpResponseMessage run(
-			@HttpTrigger(name = "req", methods = {HttpMethod.GET }, authLevel = AuthorizationLevel.ANONYMOUS, route = "tests/{hash}") 
-			HttpRequestMessage<Optional<String>> request,
+	public HttpResponseMessage run(@HttpTrigger(name = "req", methods = {
+			HttpMethod.POST }, authLevel = AuthorizationLevel.FUNCTION, route = "tests/{hash}") HttpRequestMessage<Optional<String>> request,
 			@BindingName("hash") String hash, final ExecutionContext context) {
 		context.getLogger().info("Java HTTP trigger processed a request.");
-
 		if (hash == null) {
 			return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("no hash given").build();
-		} else {
-			String uri = System.getenv("mongodb_uri");
-			if (uri==null)
-				uri="mongodb://localhost=27017/fn-test";
-			try (MongoClient client = new MongoClient(new MongoClientURI(uri))) {
-				Bson filter = new BsonDocument("_id", new BsonString(hash));
-				TestResult res = new ObjectMapper().readValue(client.getDatabase("fn-test").getCollection("testresult").find(filter).first().toJson(),TestResult.class);
-				return request.createResponseBuilder(HttpStatus.OK).body(res).build();
-			} catch ( JsonProcessingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage()).build();
-			} 
 		}
+		Optional<HttpResponseMessage> msg = request.getBody().map(b -> {
+			String uri = System.getenv("mongodb_uri");
+			if (uri == null) {
+				uri = "mongodb://localhost=27017/fn-test";
+			}
+			try (MongoClient client = new MongoClient(new MongoClientURI(uri))) {
+				BasicDBObject o = BasicDBObject.parse(b);
+				o.put("_id", hash);
+				client.getDatabase("fn-test").getCollection("testresult").insertOne(Document.parse(o.toJson()));
+				;
+				return request.createResponseBuilder(HttpStatus.OK).body(o).build();
+			} catch (Exception e) {
+				return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage()).build();
+			}
+		});
+		return msg.orElseGet(() -> request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).build());
 	}
 }
